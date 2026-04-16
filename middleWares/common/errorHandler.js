bd668404a1a079a510445a28206b009a25f0280c
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-16 07:56:15 +0600

    now error handler cando both html and json

--- middleWares/common/errorHandler.js
+++ middleWares/common/errorHandler.js
@@ -1,15 +1,24 @@
 const createError = require("http-errors");
 
-//404 not found
+//404 not found handler
 const notFoundHandler = (req, res, next) => {
   next(createError(404, "Your requested content was not found"));
 };
 
 //error handler
 const errorHandler = (err, req, res, next) => {
-  res.render("error", {
-    title: "Error page",
-  });
+  res.locals.error =
+    process.env.NODE_ENV === "development" ? err : { message: err.message };
+
+  res.status(err.status || 500);
+
+  if (res.locals.html) {
+    res.render("error", {
+      title: "Error page",
+    });
+  } else {
+    res.json(res.locals.error);
+  }
 };
 
 module.exports = {
