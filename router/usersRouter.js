a577632f9a565141b753cecc7272eac4716f8e98
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-17 06:50:14 +0600

    file uploader done

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { getUsers } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middleWares/common/decorateHtmlResponse");
+const avatarUpload = require("../middleWares/users/avatarUpload");
 
 //users page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
 
 //add user
-router.post;
+router.post("/", avatarUpload);
 
 module.exports = router;
