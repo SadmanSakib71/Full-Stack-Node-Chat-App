4733086dc5062952da0e35dee0e3b58580631a09
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-18 12:44:52 +0600

    set checklogin in the inbox page

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { getInbox } = require("../controller/inboxController");
 const decorateHtmlResponse = require("../middleWares/common/decorateHtmlResponse");
+const { checkLogin } = require("../middleWares/common/checkLogIn");
 
 //inbox page
-router.get("/", decorateHtmlResponse("Inbox"), getInbox);
+router.get("/", decorateHtmlResponse("Inbox"), checkLogin, getInbox);
 
 module.exports = router;
