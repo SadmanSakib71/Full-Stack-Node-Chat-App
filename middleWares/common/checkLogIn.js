e7e1874357d27885c354e8acf8f770e4d7a39db0
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-20 08:43:51 +0600

    create require role

--- middleWares/common/checkLogIn.js
+++ middleWares/common/checkLogIn.js
@@ -39,6 +39,7 @@ const checkLogin = (req, res, next) => {
   }
 };
 
+// redirect already logged in user to inbox pabe
 const redirectLoggedIn = function (req, res, next) {
   let cookies =
     Object.keys(req.signedCookies).length > 0 ? req.signedCookies : null;
@@ -49,6 +50,27 @@ const redirectLoggedIn = function (req, res, next) {
     res.redirect("/inbox");
   }
 };
+
+// guard to protect routes that need role based authorization
+function requireRole(role) {
+  return function (req, res, next) {
+    if (req.user.role && role.includes(req.user.role)) {
+      next();
+    } else {
+      if (res.locals.html) {
+        next(createError(401, "You are not authorized to access this page!"));
+      } else {
+        res.status(401).json({
+          errors: {
+            common: {
+              msg: "You are not authorized!",
+            },
+          },
+        });
+      }
+    }
+  };
+}
 
 module.exports = {
   checkLogin,
